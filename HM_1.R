7275b37731ee4df2f6e7c71764fccce97a8dcfe9
Gerva02 <gerva02@gmail.com>
2023-03-20 11:46:01 +0100

    layout the homework assignment

---
+++ HM_1.R
@@ -0,0 +1,16 @@
+# Homework:
+# 1) Effettuare una analisi descrittiva sul dataset "Davis" per l'altezza dichiarata
+
+# 2) Correggere l'errore che ha causato l'outlier e rieffettuare le analisi
+
+# 3) Importare il dataset cereal. Effettuare una analisi
+# descrittiva di tale dataset per familiarizzare con esso 
+# (una descrizione delle variabili puo' essere trovata qui:
+# https://www.kaggle.com/datasets/crawford/80-cereals).
+# Selezionare la variabile risposta che si ritiene piu' 
+# sensata e provare a stimare un modello di regressione
+# lineare semplice scegliendo una tra le possibili esplicative
+# quantitative presenti nel dataset. Interpetare tutti i 
+# risultati ottenuti.
+
+
